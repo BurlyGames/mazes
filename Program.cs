b34c58dd523c2d91dbd183d9fb16effd3deb94c6
Jeff Farnsworth <jeff.farnsworth@burlygames.com>
2017-07-13 01:20:48 -0700

    Cleaning up

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace Mazes
     {
         static void Main(string[] args)
         {
-            var grid = Sidewinder.Load(new DistancesGrid(10, 10));
+            var grid = Sidewinder.Load(new Grid(10, 10));
 
             Console.Write(grid.ToString());
             //Console.Read();
